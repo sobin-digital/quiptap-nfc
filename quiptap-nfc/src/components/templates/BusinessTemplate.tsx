b58b995e0bb4b5ff3d925d2c4bef75c2fc8db8e3
Sobin Siby <sobinsibydigitalmarketer@gmail.com>
2026-03-23 17:53:27 +0530

    Delete quiptap-nfc directory

--- quiptap-nfc/src/components/templates/BusinessTemplate.tsx
+++
@@ -1,7 +0,0 @@
-import type { PublicProfileData } from "./PublicTemplateCard";
-import PublicTemplateCard from "./PublicTemplateCard";
-
-export default function BusinessTemplate(props: { profile: PublicProfileData }) {
-  return <PublicTemplateCard variant="business" templateId={null} profile={props.profile} />;
-}
-
--- quiptap-nfc/src/components/templates/MinimalTemplate.tsx
+++
@@ -1,7 +0,0 @@
-import type { PublicProfileData } from "./PublicTemplateCard";
-import PublicTemplateCard from "./PublicTemplateCard";
-
-export default function MinimalTemplate(props: { profile: PublicProfileData }) {
-  return <PublicTemplateCard variant="minimal" templateId={null} profile={props.profile} />;
-}
-
